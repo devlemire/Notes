73bf512f508d55bfd25f3d468805454fc7d42026
James Lemire <webdev.jameslemire@gmail.com>
2018-10-17 13:30:17 -0600

    Update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,69 +1,71 @@
-var selection = ["Rock", "Paper", "Scissors"];  //Used for displaying choice in console.log
-var player = playerChoice();  //Store the player's choice in the global variable player using the function playerChoice
-var computer = computerChoice();  //Store the computer's choice in the global variable computer using the function computerChoice
-
-console.log(compare(player, computer));  //Compare the choices from the player and the computer, then determine a winner and log it for the user
+const options = ["Rock", "Paper", "Scissors"];
 
 function playerChoice() {
-	var choice;  //Local var to hold the choice from the player
-	 do {  //I used a do-while so that the prompt would be asked at least once
-        choice = prompt("Rock(1), Paper(2), or Scissors(3)?").toLowerCase();  //Ask the user through prompt for their choice of Rock, Paper, or Scissors and store in the local variable choice
-        switch(choice) {  //Convert their choice to usable data if the user entered "rock", "paper", or "scissors"
-        	case 'rock':
-        		choice = 1;  //"rock" is the same as 1 ("rock" = 1)
-        		break;
-        	case 'paper':
-        		choice = 2;  //"paper" is the same as 2 ("paper" = 2)
-        		break;
-        	case 'scissors':
-        		choice = 3;  //"scissors" is the same as 3 ("scissors" = 3)
-        		break;
-        	default:
-        		choice = parseInt(choice);  //If the user doesn't enter rock, paper, or scissors, default to converting their answer to an integer
-        }
-    } while (choice !== 1 && choice !== 2 && choice !== 3);  //Continue to ask for choice if choice does not equal 1, 2, or 3
-  
-	console.log("Player Choice: " + selection[choice - 1] + ".")  //Log information to the user using the selection array
-	return choice;  //Return the numeric choice value to the global variable player
+  let choice;
+
+  do {
+    choice = prompt("Rock(1), Paper(2), or Scissors(3)?").toLowerCase();
+
+    switch (choice) {
+      case "rock":
+        choice = 1;
+        break;
+      case "paper":
+        choice = 2;
+        break;
+      case "scissors":
+        choice = 3;
+        break;
+      default:
+        choice = parseInt(choice);
+    }
+  } while (choice !== 1 && choice !== 2 && choice !== 3);
+
+  console.log(`Player Choice: ${options[choice - 1]}.`);
+  return choice;
 }
 
 function computerChoice() {
-	var choice = Math.floor(Math.random() * 3 + 1);  //Generates a number, 1 - 3 (rock = 1, paper = 2, scissors = 3)
-	console.log("Computer Choice: " + selection[choice - 1] + ".");  //Log information to the user using the selection array
-	return choice;  //Return the numeric choice value to the global variable computer
+  const choice = Math.floor(Math.random() * 3 + 1);
+  console.log(`Computer Choice: ${options[choice - 1]}.`);
+  return choice;
 }
 
-function compare(player, computer) {  //Use the two numbers (player & computer) to determine a winner through switch statements
-	switch(player) {
-		case 1:  //Player chose rock
-			switch(computer) {
-				case 1:  //Computer chose rock
-					return "The game resulted in a tie.";  //Return the winner for console.log
-				case 2:  //Computer chose paper
-					return "The computer won.";  //Return the winner for console.log
-				case 3:  //Computer chose scissors
-					return "You won.";  //Return the winner for console.log
-			}
-			break;
-		case 2:  //Player chose paper
-			switch(computer) {
-				case 1:  //Computer chose rock
-					return "You won.";  //Return the winner for console.log
-				case 2:  //Computer chose paper
-					return "The game resulted in a tie.";  //Return the winner for console.log
-				case 3:  //Computer chose scissors
-					return "The computer won.";  //Return the winner for console.log
-			}
-			break;
-		case 3:  //Player chose scissors
-			switch(computer) {
-				case 1:  //Computer chose rock
-					return "The computer won.";  //Return the winner for console.log
-				case 2:  //Computer chose paper
-					return "You won.";  //Return the winner for console.log
-				case 3:  //Computer chose scissors
-					return "The game resulted in a tie.";  //Return the winner for console.log
-			}
-			break;
-	} 
+function compare(player, computer) {
+  switch (player) {
+    case 1:
+      switch (computer) {
+        case 1:
+          return "The game resulted in a tie.";
+        case 2:
+          return "The computer won.";
+        case 3:
+          return "You won.";
+      }
+      break;
+    case 2:
+      switch (computer) {
+        case 1:
+          return "You won.";
+        case 2:
+          return "The game resulted in a tie.";
+        case 3:
+          return "The computer won.";
+      }
+      break;
+    case 3:
+      switch (computer) {
+        case 1:
+          return "The computer won.";
+        case 2:
+          return "You won.";
+        case 3:
+          return "The game resulted in a tie.";
+      }
+      break;
+  }
+}
+
+function play() {
+  console.log(compare(playerChoice(), computerChoice()));
 }
